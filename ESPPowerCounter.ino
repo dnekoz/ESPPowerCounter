fcf390fa42c53e9e150a5f84209dc48f16aeb225
Denis Nekoz <dnekoz@gmail.com>
2019-04-23 18:25:02 +0300

    Подключен дисплей 1602

--- ESPPowerCounter.ino
+++ ESPPowerCounter.ino
@@ -1,3 +1,6 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
 #define PIN_PHOTO_SENSOR_DIGITAL D5
 
 int koef = 5000; // Количество вспышек на 1 кВт
@@ -9,8 +12,19 @@ volatile float freq = 0;             // Частота вспышек
 volatile float currPower = 0;        // Мгновенная мощность
 float power = 0;                     // Общее энергопотребление до перезагрузки
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16,2);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("test");
+  lcd.setCursor(0, 1);
+  lcd.print("LCD");
+
+  Serial.println("...ok...");  
   pinMode(PIN_PHOTO_SENSOR_DIGITAL, INPUT_PULLUP);
   attachInterrupt (digitalPinToInterrupt (PIN_PHOTO_SENSOR_DIGITAL), blink, RISING);
 }
@@ -22,6 +36,12 @@ void loop() {
     currPower = 0;    // Получим значеение мгновенной мощности в кВт = 0
     }
   Serial.print(impulseCounter);Serial.print("  ");Serial.print(freq,3); Serial.print("  "); Serial.print(currPower,3); Serial.print("  "); Serial.println((float)impulseCounter/koef,3);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(impulseCounter);
+  lcd.setCursor(0, 1);
+  lcd.print(impulseCounter/koef);
+  
   delay(1000); // Статус 1 раз в секунду
 }
 
